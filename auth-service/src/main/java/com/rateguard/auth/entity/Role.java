4990949be7216d488d00ce9e23331713ba0f7dfb
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2026-04-02 23:06:31 +0530

    Create auth service

---
+++ auth-service/src/main/java/com/rateguard/auth/entity/Role.java
@@ -0,0 +1,6 @@
+package com.rateguard.auth.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ auth-service/src/main/java/com/rateguard/auth/entity/User.java
@@ -0,0 +1,41 @@
+package com.rateguard.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
